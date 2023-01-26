3d5f8aaac4bb86a41e5c38b7dc0099c42793e67f
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-01-26 09:01:44 -0300

    first exercise

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,6 +1,10 @@
 package com.example;
 
 public class Main {
+    /**
+     * Este metodo se ejecutará al inicializarse la clase o al ser ejecutada
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/com/first/Main.java
@@ -0,0 +1,25 @@
+package com.first;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esta es el inicio de todo");
+
+        // Tipos de datos
+        // Numericos Enteros
+        byte num1 = 127;
+        short num2 = 5000;
+        int num3 = 10000000;
+        long num4 = 1231231;
+
+        // Numericos Decimales
+        float num5 = 10.5f;
+        double num6 = 11.55d;
+
+        // Valores de verdad
+        boolean verdadero = false;
+
+        // Texto
+        char text1 = 'A';
+        String tex2 = "Probando";
+    }
+}
